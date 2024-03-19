92c1420ea82380c4f43fbafbbdd90f0851809b1e
Hugo Quinteiro <hugosantista@gmail.com>
2024-03-19 18:04:05 -0300

    mudancas

--- src/pages/basicos/caixa.tsx
+++ src/pages/basicos/caixa.tsx
@@ -4,8 +4,8 @@ export default function PaginaCaixa() {
     return (
         <div className='flex gap-7 p-7'>
             <Caixa>Teste 1</Caixa>
-            <Caixa>Teste 2</Caixa>
-            <Caixa>Teste 3</Caixa>
+            <Caixa>Teste de caixa com muito código</Caixa>
+            <Caixa>Caixa 3</Caixa>
         </div>
     )
 }
